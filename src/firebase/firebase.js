ee61d995d23cde1d1c60064714010e0f9e011aaa
lajawanti <lajawanti.dhake@gmail.com>
2020-11-30 17:45:25 -0500

    firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,10 +1,11 @@
+
 const firebaseConfig = {
-      apiKey: REACT_APP_APIKEY,
-      authDomain: REACT_APP_AUTHDOMAIN,
-      databaseURL: REACT_APP_DATABASEURL,
+      apiKey: process.env.REACT_APP_APIKEY,
+      authDomain: process.env.REACT_APP_AUTHDOMAIN,
+      databaseURL: process.env.REACT_APP_DATABASEURL,
       projectId:  process.env.REACT_APP_PROJECTID,
-      storageBucket: REACT_APP_STORAGEBUCKET,
-      messagingSenderId: REACT_APP_MESSAGINGSENDERID,
-      appId: REACT_APP_APPID,
-      measurementId: REACT_APP_MEASUREMENTID
+      storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+      appId: process.env.REACT_APP_APPID,
+      measurementId: process.env.REACT_APP_MEASUREMENTID
 };
